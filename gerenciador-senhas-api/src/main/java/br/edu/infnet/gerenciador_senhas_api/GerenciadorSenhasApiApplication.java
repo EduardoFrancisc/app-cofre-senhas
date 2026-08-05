d6cdc2e0c6358193a2abadb22af7269d4e2aa87b
EduardoFrancisc <duudu.0304@gmail.com>
2026-08-05 18:39:30 -0300

    iniciar a api e testar a conexão com o banco postgres local

---
+++ gerenciador-senhas-api/src/main/java/br/edu/infnet/gerenciador_senhas_api/GerenciadorSenhasApiApplication.java
@@ -0,0 +1,12 @@
+package br.edu.infnet.gerenciador_senhas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorSenhasApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorSenhasApiApplication.class, args);
+	}
+
+}
---
+++ gerenciador-senhas-api/src/test/java/br/edu/infnet/gerenciador_senhas_api/GerenciadorSenhasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.gerenciador_senhas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorSenhasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
